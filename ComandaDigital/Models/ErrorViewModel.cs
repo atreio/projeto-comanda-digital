292bbb724396a4f10f433ddb594488cc74477386
luciano <luciano@perroni.tech>
2019-09-11 08:54:44 -0300

    Adicionar arquivos de projeto.

---
+++ ComandaDigital/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComandaDigital.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
