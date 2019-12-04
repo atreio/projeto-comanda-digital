ebadaea40094e4d412e00037f7e3d1d61fdd9647
atreio <luciano.s.rocha@outlook.com>
2019-12-04 14:29:17 -0300

    pagamento

---
+++ ComandaDigital/Controllers/ContasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComandaDigital.Controllers
+{
+    [Route("Contas/[controller]")]
+    public class ContasController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ComandaDigital/Models/Pagamento.cs
@@ -0,0 +1,15 @@
+﻿using ComandaDigital.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComandaDigital.Models
+{
+    public class Pagamento : IEntity
+    {
+        public Pagamento() { }
+
+
+    }
+}
